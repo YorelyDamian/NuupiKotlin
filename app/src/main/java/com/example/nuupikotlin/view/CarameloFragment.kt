7dea83077c430f7287f443054f9dd9b2ebc0aff4
YorelyDamian <yorelyGuadalupeMtzDmn@gmail.com>
2022-12-28 15:23:23 -0600

    ImagenPersonalizada

--- app/src/main/java/com/example/nuupikotlin/view/CarameloFragment.kt
+++ app/src/main/java/com/example/nuupikotlin/view/CarameloFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nuupikotlin.view
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,12 +8,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.nuupikotlin.R
 import com.example.nuupikotlin.databinding.FragmentCarameloBinding
+import com.github.dhaval2404.imagepicker.ImagePicker
+import de.hdodenhof.circleimageview.CircleImageView
+import java.io.File
 
 class CarameloFragment : Fragment() {
 
+    var circleImageUser: CircleImageView? = null
+    private var imageFile: File? = null
     private lateinit var binding: FragmentCarameloBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?):
@@ -49,6 +58,41 @@ class CarameloFragment : Fragment() {
             spColor.adapter = adapter
         }
 
+        circleImageUser = view.findViewById(R.id.circleImage_User)
+
+        circleImageUser?.setOnClickListener{ selectImage() }
     }
 
+    private val startImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+
+        val resultCode = result.resultCode
+        val data = result.data
+
+        if(resultCode == Activity.RESULT_OK){
+            val fileUri = data?.data
+            imageFile = File(fileUri?.path)
+            circleImageUser?.setImageURI(fileUri)
+        }
+        else if(resultCode == ImagePicker.RESULT_ERROR){
+            Toast.makeText(context, ImagePicker.getError(data), Toast.LENGTH_LONG).show()
+        }
+        else{
+            Toast.makeText(context,"Tarea se cancelo", Toast.LENGTH_LONG).show()
+        }
+
+    }
+
+    private fun selectImage() {
+        ImagePicker.with(this)
+            .crop()                    //Crop image(Optional), Check Customization for more option
+            .compress(1024)            //Final image size will be less than 1 MB(Optional)
+            .maxResultSize(
+                1080,
+                1080
+            )    //Final image resolution will be less than 1080 x 1080(Optional)
+            .createIntent { intent ->
+                startImageResult.launch(intent)
+            }
+    }
 }
