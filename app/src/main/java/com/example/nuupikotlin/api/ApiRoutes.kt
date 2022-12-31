1e29acdb49c14df77d5b57e2913781f25be868ff
YorelyGMD <yorelyGuadalupeMtzDmn@gmial.com>
2022-12-30 18:39:11 -0600

    Api

--- app/src/main/java/com/example/nuupikotlin/api/ApiRoutes.kt
+++ app/src/main/java/com/example/nuupikotlin/api/ApiRoutes.kt
@@ -4,8 +4,8 @@ import com.example.nuupikotlin.routes.UsersRoutes
 
 class ApiRoutes {
 
-    val API_URL="http://localhost:4000/api/Nuupi/"
-    //val API_URL="https://apirestnuupi-production.up.railway.app/api/Nuupi/"
+    //val API_URL="http://localhost:4000/api/Nuupi/"
+    val API_URL="https://apirestnuupi-production.up.railway.app/api/Nuupi/"
     val retrofit = RetrofitClient()
     fun getUsersRoutes(): UsersRoutes {
         return retrofit.getClient(API_URL).create(UsersRoutes::class.java)
